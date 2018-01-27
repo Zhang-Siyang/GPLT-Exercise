b1e9bf4f082d66cd4027e2213524057d7d500ff3
Zhang-Siyang <CE.BB.CAT@GMAIL.COM>
2018-01-27 13:54:22 +0800

    add comment of code, a little improvement in permance

--- L1-002.c
+++ L1-002.c
@@ -4,22 +4,17 @@ int get_lines(int sum);
 int putALotChar(int n, char ch);
 
 int main(){
-    int num, numOfChar, totalLines;
-    char ch;
+    int num, numOfChar; /*num ~ number of char, numOfChar ~ number of char for every line*/
+    char ch;    /*what should be show as char*/
     scanf("%d %c", &num, &ch);
-    int const line = get_lines(num);
-    int const totalLines = line * 2 + 1;
+    int const line = get_lines(num);    /*a haif of height*/
+    int const totalLines = line * 2 + 1;    /*height of funnel*/
 
     for(int level = 0; level < totalLines; level++){
         if(level < line){
             numOfChar = totalLines - (level * 2);
             putALotChar(level, ' ');
             putALotChar(numOfChar, ch);
-            putchar('\n');
-        } else if(level == line){
-            numOfChar = 1;
-            putALotChar(level, ' ');
-            putchar(ch);
             putchar('\n');
         } else {
             numOfChar = (level - line) * 2 + 1;
@@ -35,7 +30,7 @@ int main(){
 }
 
 int get_lines(int sum){
-    /*返回上(下)部分的行数*/
+    /*return half of the height of the funnel*/
     sum = sum - 1;
     int line = 0;
     for(int num_of_char = 6; (sum - num_of_char) >= 0; num_of_char += 4){
@@ -46,7 +41,21 @@ int get_lines(int sum){
 }
 
 int putALotChar(int n, char ch){
+    /*just like it's name, put a lot char*/
     for(; n > 0; n--)
         putchar(ch);
     return n;
 }
+
+
+/*------
+输入样例：
+19 *
+输出样例：
+*****
+ ***
+  *
+ ***
+*****
+2
+------*/
