41d7a07f4b2a8c9026e1b3214884ebf7a32932c3
Zhang-Siyang <CE.BB.CAT@GMAIL.COM>
2018-02-07 11:46:38 +0800

    到底有多二

---
+++ L1-017.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    char num[51], i = 0, hc = 0, len = 0;
+    float beishu = 1;
+    scanf("%s", &num);
+    for(i = 0; num[i] != '\0'; i++){
+        if(num[i] == '2'){
+            hc++;
+            printf("#num[%d] == 2!#", i);
+        }
+    }
+    len = i;
+    if(len == 0)
+        return 1;
+    if(num[0] == '-'){
+        beishu *= 1.5;
+        len--;
+    }
+    if( (1&num[i - 1]) == 0)
+        beishu *= 2;
+    printf("%.2f%%", beishu * 100 * hc / len);
+    return 0;
+}
