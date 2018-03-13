da4f4671db82e5abadd78c09f1e0a97cfd37179f
Zhang-Siyang <CE.BB.CAT@Gmail.com>
2018-03-13 19:05:26 +0800

    L1-025

---
+++ L1-025.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int in;
+    while((in = getchar()) != EOF){
+        if(isgigtal(in)){
+            a = a * 10 + in - '0';
+            continue;
+        } else 
+    }
+}
